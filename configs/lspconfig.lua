285493c7e87413d82c4e27dfb96df3239f9acb02
Abhijeet Vashistha <abhivashistha5@gmail.com>
2023-06-13 22:57:21 +0530

    inital config

---
+++ configs/lspconfig.lua
@@ -0,0 +1,30 @@
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+
+local lspconfig = require("lspconfig")
+local util = require "lspconfig/util"
+
+lspconfig.rust_analyzer.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = { "rust" },
+  root_dir = util.root_pattern("Cargo.toml"),
+  settings = {
+    ['rust-analyzer'] = {
+      cargo = {
+        allFeatures = true
+      },
+    },
+  },
+})
+
+lspconfig.tsserver.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+})
+
+-- lspconfig.luals.setup({
+--   on_attach = on_attach,
+--   capabilities = capabilities,
+--   filetypes = { "lua" }
+-- })
