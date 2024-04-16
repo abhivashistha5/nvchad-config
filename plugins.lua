23aebbd9029b6f7cce7aa3f822344d69329c1d5a
Abhijeet Vashistha <abhivashistha5@gmail.com>
2024-04-16 21:16:05 +0530

    Removed null ls and fixed failed plugin load

--- plugins.lua
+++ plugins.lua
@@ -21,7 +21,7 @@ local plugins = {
     dependencies = {
       "nvimtools/none-ls.nvim",
       config = function()
-        require "custom.configs.null-ls"
+        -- require "custom.configs.null-ls"
       end,
     },
     config = function()
@@ -53,6 +53,7 @@ local plugins = {
       require("core.utils").load_mappings "toggleterm"
     end,
   },
+  { "nvim-neotest/nvim-nio" },
 
   -- rust --
   {
@@ -139,7 +140,7 @@ local plugins = {
         -- node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
         debugger_path = os.getenv "HOME" .. "/.debugger/vscode-js-debug", -- Path to vscode-js-debug installation.
         -- debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
-        adapters = { "pwa-node", "pwa-chrome" }, -- which adapters to register in nvim-dap
+        adapters = { "pwa-node", "pwa-chrome" },                          -- which adapters to register in nvim-dap
         -- log_file_path = "(stdpath cache)/dap_vscode_js.log" -- Path for file logging
         -- log_file_level = false -- Logging level for output to file. Set to false to disable file logging.
         -- log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
