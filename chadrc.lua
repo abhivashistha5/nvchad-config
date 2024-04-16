82c42025d5e131529f6599883210765089a55a51
Abhijeet Vashistha <abhivashistha5@gmail.com>
2024-04-16 22:59:21 +0530

    telescope custom config to ignore target build files

--- chadrc.lua
+++ chadrc.lua
@@ -1,12 +1,14 @@
----@type ChadrcConfig 
+---@type ChadrcConfig
 local M = {}
-M.ui = {theme = 'nightfox'}
+M.ui = {
+  theme = 'nightfox',
+}
 M.plugins = 'custom.plugins'
 M.mappings = require 'custom.mappings'
+
 
 -- vim options
 local opt = vim.opt
 opt.relativenumber = true
 
 return M
-
--- plugins.lua
+++ plugins.lua
@@ -147,6 +147,33 @@ local plugins = {
       }
     end,
   },
+
+
+  -- telescope
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    cmd = "Telescope",
+    init = function()
+      require("core.utils").load_mappings "telescope"
+    end,
+    opts = function()
+      local options = require "plugins.configs.telescope"
+      local custom_options = require "custom.configs.telescope"
+      options.defaults.file_ignore_patterns = custom_options.defaults.file_ignore_patterns
+      return options
+    end,
+    config = function(_, opts)
+      dofile(vim.g.base46_cache .. "telescope")
+      local telescope = require "telescope"
+      telescope.setup(opts)
+
+      -- load extensions
+      for _, ext in ipairs(opts.extensions_list) do
+        telescope.load_extension(ext)
+      end
+    end,
+  },
 }
 
 return plugins
