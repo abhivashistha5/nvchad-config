b8072dffd5e44b60e4e49ea48d42dc9fe4586ffb
Abhijeet Vashistha <abhivashistha5@gmail.com>
2024-04-18 19:26:48 +0530

    Added none-ls for autoformat on save typescript files

--- plugins.lua
+++ plugins.lua
@@ -18,7 +18,12 @@ local plugins = {
   },
   {
     "neovim/nvim-lspconfig",
-    config = function()
+    dependencies = {
+      "nvimtools/none-ls.nvim",
+      config = function()
+        require "custom.configs.null-ls"
+      end,
+    },config = function()
       require "plugins.configs.lspconfig"
       require "custom.configs.lspconfig"
     end,
