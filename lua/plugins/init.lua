f07b298157039a9cd6eea58fbeeec1dddc948440
Abhijeet Vashistha <22078423+abhivashistha5@users.noreply.github.com>
2025-12-21 22:44:29 +0530

    enabled flutter plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -137,6 +137,15 @@ return {
   --     require("telescope").load_extension "flutter"
   --   end,
   -- },
+  {
+    "nvim-flutter/flutter-tools.nvim",
+    lazy = false,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "stevearc/dressing.nvim", -- optional for vim.ui.select
+    },
+    config = true,
+  },
   --
   -- -- typescript
   -- {
